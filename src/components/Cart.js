31a810a48421b9f3976ab4f510ad5f7a70d6c3db
ugochukwu onyekanma <ugochikanele@gmail.com>
2022-03-30 21:39:17 +0100

    corrected cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,8 +6,8 @@ import CartItem from './CartItem'
 function Cart({cart, price, discount, emptyCart }) {
  
   return (  
-    <div className='h-60 w-60 z-30 mx-16 mt-16 bg-white fixed rounded-2xl'>
-        <h4 className='mx-10 my-5 font-bold'>Cart</h4>
+    <div className='h-72 w-[350px] z-30 mx-3 mt-16 bg-white fixed rounded-2xl'>
+        <h4 className='mx-10 my-10 font-bold'>Cart</h4>
         <hr/>
         {
           cart.length ? 
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -7,15 +7,15 @@ function CartItem({ bin, shoe, cart, price, discount, emptyCart }) {
 
   return (
     <div>
-       <div className='flex items-center mt-10 justify-around'>
+       <div className='flex items-center mt-5 justify-around'>
             <img className='w-12 h-12 rounded-2xl' src={shoe} alt='' />
             <div>
-                <h4 className='text-sm'>Autumn limited edition...</h4>
-                <span>${actualPrice} x {cart.length} ${totalPrice}</span>
+                <h4 className='text-lg'>Autumn limited edition...</h4>
+                <span className='text-lg'>${actualPrice} x {cart.length} <strong>${totalPrice}</strong></span>
             </div>
             <img onClick={()=>emptyCart()} src={bin} alt='' />
         </div>
-        <button className='bg-orange-500 w-52 ml-4 mt-10 rounded-md text-white'>checkout</button> 
+        <button className='bg-orange-500 w-80 h-10 ml-4 mt-10 rounded-md text-white'>checkout</button> 
     </div>
   )
 }
