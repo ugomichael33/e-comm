6247d66d47e5e890d296a0602cf86d26e46302c6
ugochukwu onyekanma <ugochikanele@gmail.com>
2022-03-30 21:49:41 +0100

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,16 +6,16 @@ function Sidebar({handleToggle}) {
 
   return (
       <div>
-         <div className='w-3/4 h-full bg-white fixed z-10'>
+         <div className='w-3/4 h-full bg-white fixed z-20'>
             <div>
-                <AiOutlineClose onClick={handleToggle} className='mt-3 ml-4 cursor-pointer'/>
+                <AiOutlineClose onClick={handleToggle} className='mt-3 ml-4 cursor-pointer text-lg'/>
             </div>
-                <ul className='ml-7 py-5 ease-linear duration-[10000ms]' onClick={handleToggle}>
-                <li>Collections</li>
-                <li>Men</li>
-                <li>Women</li>
-                <li>About</li>
-                <li>Contact</li>
+            <ul className='ml-7 py-10 ease-linear duration-[10000ms] ' onClick={handleToggle}>
+                <li className='my-2 text-lg font-bold'>Collections</li>
+                <li className='text-lg font-bold'>Men</li>
+                <li className='my-2 text-lg font-bold'>Women</li>
+                <li className='text-lg font-bold'>About</li>
+                <li className='my-2 text-lg font-bold'>Contact</li>
             </ul>   
         </div>
     </div>
